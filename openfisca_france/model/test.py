630b12c5d582d953c759a78786700862474f9125
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-02-04 23:52:16 -0500

    Document work around

--- openfisca_france/model/test.py
+++ openfisca_france/model/test.py
@@ -12,5 +12,17 @@ class is_zone_1(Variable):
     definition_period = MONTH
 
     def formula(menage, period, parameters):
-        # Toujours faux, car TypesZoneApl importé depuis aides_logement et TypesZoneApl du type de zone_apl sont 2 énums distincs
+        # Cette formule ne fonctione pas, et retourne toujours faux, car TypesZoneApl importé depuis aides_logement et TypesZoneApl du type de zone_apl sont 2 énums distinctes
         return menage('zone_apl', period) == TypesZoneApl.zone_1
+
+
+class is_zone_2(Variable):
+    value_type = bool
+    entity = Menage
+    definition_period = MONTH
+
+    def formula(menage, period, parameters):
+        # Workaround avec possible_values
+        zone_apl = menage('zone_apl', period)
+        TypesZoneApl = zone_apl.possible_values
+        return zone_apl == TypesZoneApl.zone_2
