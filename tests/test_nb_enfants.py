363d31ccb4e5742576d30cfeb3546d8065e0a3e3
Laurent Bossavit <laurent.git@bossavit.com>
2019-03-24 20:25:12 +0100

    Adapte France au refactoring des entités

--- tests/test_nb_enfants.py
+++ tests/test_nb_enfants.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+from openfisca_core.periods import period
 from openfisca_core.tools import assert_near
 
 from .test_entities import TEST_CASE_AGES, new_simulation
@@ -12,10 +13,10 @@ def test_nb_enfants():
     simulation = tax_benefit_system.new_scenario().init_from_dict(test_case).new_simulation()
     from openfisca_france.model.prestations.prestations_familiales.base_ressource import nb_enf
 
-    assert_near(nb_enf(simulation.famille, simulation.period.first_month, 3, 18), [2, 0])
-    assert_near(nb_enf(simulation.famille, simulation.period.first_month, 19, 50), [0, 1])  # Adults don't count
+    assert_near(nb_enf(simulation.famille, period('2013-01'), 3, 18), [2, 0])
+    assert_near(nb_enf(simulation.famille, period('2013-01'), 19, 50), [0, 1])  # Adults don't count
 
     test_case['individus']['ind5']['autonomie_financiere'] = True
     simulation_2 = new_simulation(test_case)
 
-    assert_near(nb_enf(simulation_2.famille, simulation_2.period.first_month, 19, 50), [0, 0])
+    assert_near(nb_enf(simulation_2.famille, period('2013-01'), 19, 50), [0, 0])
